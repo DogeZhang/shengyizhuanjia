a411fe0a597abd9c404bdd0ee3b6ad71562dc808
DogeZhang <zhangyz86@hotmail.com>
2019-12-20 21:50:27 +0800

    第六次

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,10 +2,18 @@ import { ZyzModule } from './../../shared/components/zyz.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  }
+];
 
 @NgModule({
   imports: [
