03c6a33e7e16883120d5db953c1c801c41be414a
DogeZhang <zhangyz86@hotmail.com>
2019-12-20 16:12:48 +0800

    第五次：添加登录验证（localStorageService）

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,3 +1,4 @@
+import { LocalStorageService } from './../../shared/services/local-storage.service';
 import { Component, OnInit } from '@angular/core';
 import { ToastController, AlertController } from '@ionic/angular';
 import { NgForm } from '@angular/forms';
@@ -10,11 +11,19 @@ import { NgForm } from '@angular/forms';
 export class LoginPage implements OnInit {
   username: string="";
   password: string="";
+  userInfo={
+    phone: '',
+    email: '',
+    shopName: '',
+    password: '',
+    confirmPassword: '',
+    code: ''
+  };
 
   openForgotPassword() {
     console.log("function: openForgotPassword activated!");
   }
-  constructor(private toastController: ToastController, private alertCtrl: AlertController) { }
+  constructor(private toastController: ToastController, private alertCtrl: AlertController, private localStorageService: LocalStorageService) { }
 
   ngOnInit() {
   }
@@ -32,19 +41,34 @@ export class LoginPage implements OnInit {
       message: '手机号或邮箱的格式错误',
       duration: 3000
     });
-    let phoneNumNULL = await this.toastController.create({
+    let phoneNumNull = await this.toastController.create({
       message: '请输入您的手机号码或者邮箱',
       duration: 3000
     });
+    let passwordNull = await this.toastController.create({
+      message: '密码不能为空',
+      duration: 3000
+    })
 
     if(this.username == "" || this.username == null) {
-      phoneNumNULL.present();
+      phoneNumNull.present();
       return;
     }
     if(phoneReg.test(this.username)||emailReg.test(this.username)) {
     } else {
       phoneNumWrong.present();
       return;
+    }
+    if(this.password == "" || this.password == null) {
+      passwordNull.present();
+      return;
+    }
+    // console.log(this.localStorageService.get('userInfo', JSON));
+    this.userInfo = this.localStorageService.get('userInfo', JSON);
+    if((this.userInfo.phone == this.username||this.userInfo.email == this.username)&&(this.userInfo.password == this.password)) {
+      console.log("验证通过！");
+    } else {
+      console.log("输入的username或password错误！");
     }
   }
 }
